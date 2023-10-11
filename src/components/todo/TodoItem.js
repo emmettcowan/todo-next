4fff6c845127377048c0337773ea706bf28b3e99
Emmett Cowan <knockanillaun@gmail.com>
2023-10-11 16:34:08 +0100

    fixed key list item warning

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -2,22 +2,17 @@ import React from "react";
 
 export default function TodoItem({ entry, handleDelete }) {
   return (
-    <div className=" flex p-4 bg-gray-500 my-3 rounded-lg shadow-xl">
+    <div className="flex p-4 bg-gray-500 my-3 rounded-lg shadow-xl">
       <ul className="w-full">
         {entry &&
-          entry.map(({ id, text }) => {
-            return (
-              <>
-                <li className="flex w-full" key={id}>
-                  <p className="flex w-full">{text} </p>
-                  <input className="h-6 w-6 m-2" type="checkbox" name="checkbox" id="checkbox" />
-                  <button className="h-6 w-6 m-2" onClick={() => handleDelete(id)}>x</button>
-                </li>
-              </>
-            )
-          })}
+          entry.map(({ id, text }) => (
+            <li key={id} className="flex w-full">
+              <p className="flex w-full">{text}</p>
+              <input className="h-6 w-6 m-2" type="checkbox" name="checkbox" id="checkbox" />
+              <button className="h-6 w-6 m-2" onClick={() => handleDelete(id)}>x</button>
+            </li>
+          ))}
       </ul>
-
     </div>
   )
 }
