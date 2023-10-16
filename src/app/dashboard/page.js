0b6901295187a517f292f1531854a90161a54e72
Emmett Cowan <knockanillaun@gmail.com>
2023-10-16 17:33:33 +0100

    filled out homepage

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -10,6 +10,7 @@ export default async function Dashboard() {
     <AuthCheck>
       <main className="flex  flex-col items-center p-5 h-screen">
         <div>
+          <h1 className="text-zinc-700 ml-24 font-extrabold text-4xl">Hello, {session.user.name}</h1>
           <Todo session={session} />
         </div>
       </main>
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
-
+import Image from "next/image";
+import todoPic from "./public/todoApp.jpg";
 
 export default function Home() {
   return (
-    <main className="bg-white flex flex-col px-20 items-center h-screen">
-      <h1 className="text-3xl p-24 font-bold text-zinc-800">Home</h1>
+    <main className="bg-gray-200  flex flex-col px-20 items-center h-screen">
+      <div className="flex m-24">
+        <div>
+          <p className="text-zinc-700 font-bold text-4xl mr-16">This is a simple app with a todo list</p>
+          <p className="text-zinc-700 text-xl mt-16 mr-16">Login to go to the dashboard and start adding tasks</p>
+        </div>
+        <Image
+          src={todoPic}
+          width={500}
+          height={500}
+          alt="Picture of Todo App" />
+      </div>
     </main>
   )
 }
