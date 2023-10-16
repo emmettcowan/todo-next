0834cd3c58b9e58ec39d8793b010210c54f48895
Emmett Cowan <knockanillaun@gmail.com>
2023-10-16 17:39:20 +0100

    Removed Authcheck from dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,19 +1,21 @@
 import { getServerSession } from "next-auth";
 import Todo from "@/components/todo/Todo"
-import AuthCheck from "@/components/AuthCheck";
 
 
 export default async function Dashboard() {
   const session = await getServerSession();
 
-  return (
-    <AuthCheck>
-      <main className="flex  flex-col items-center p-5 h-screen">
-        <div>
-          <h1 className="text-zinc-700 ml-24 font-extrabold text-4xl">Hello, {session.user.name}</h1>
-          <Todo session={session} />
-        </div>
-      </main>
-    </AuthCheck>
+  if (session) return (
+    <main className="flex  flex-col items-center p-5 h-screen">
+      <div>
+        <h1 className="text-zinc-700 ml-24 font-extrabold text-4xl">Hello, {session.user.name}</h1>
+        <Todo session={session} />
+      </div>
+    </main>
+  );
+  if (!session) return (
+    <main className="flex  flex-col items-center p-5 h-screen">
+      <h1 className="text-zinc-700 ml-24 font-extrabold text-4xl text mt-16">Please login to view the dashboard</h1>
+    </main>
   );
 }
